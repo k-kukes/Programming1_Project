798c42689149930fef9f95918ebd500345075ce6
k-kukes <kukes.kanthasamy@gmail.com>
2024-05-25 11:53:11 -0400

    Updated Class Department

--- src/main/java/org/example/Department.java
+++ src/main/java/org/example/Department.java
@@ -1,7 +1,37 @@
 package org.example;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@EqualsAndHashCode
+@Getter
+@Setter
+
 public class Department {
     private String departmentId;
     private String departmentName;
     private static int nextId;
+
+    public Department(String departmentId, String departmentName) {
+        if (validateDepartmentName(departmentName)) {
+            this.departmentId = departmentId;
+            this.departmentName = departmentName;
+        } else {
+            this.departmentId = null;
+            this.departmentName = null;
+        }
+    }
+
+    public static boolean validateDepartmentName(String departmentName) {
+        for (int i = 0; i < departmentName.length(); i++) {
+            char c = departmentName.charAt(i);
+            if (!Character.isLetter(c) && c != ' ') {
+                return false;
+            }
+        }
+        return true;
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,8 +2,8 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        Address address1 = new Address(10, "spring", "mtl", "qc", "a2d 3r5", "canada");
-        String postalCode = "a2d3r5";
-        System.out.println(address1);
+        Address address1 = new Address(10, "spring", "mtl", "qc", "a2d3r5", "canada");
+        Department department1 = new Department("2d", "Kukes K");
+        System.out.println(department1);
     }
 }
